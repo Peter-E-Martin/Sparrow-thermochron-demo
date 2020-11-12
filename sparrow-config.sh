8434de4d0dafb66dc05d32e3450a5e8b47e4f77e
Daven Quinn <dev@davenquinn.com>
2020-11-12 02:53:48 -0600

    Updates to configuration

--- sparrow-config.sh
+++ sparrow-config.sh
@@ -1,18 +1,17 @@
 # Configures environment for TRaIL lab
 
-export PROJECT_DIR="${0:h}"
-export SPARROW_PATH="$PROJECT_DIR/Sparrow"
-export SPARROW_BACKUP_DIR="$PROJECT_DIR/backups"
+export SPARROW_PATH="$SPARROW_CONFIG_DIR/Sparrow"
+export SPARROW_BACKUP_DIR="$SPARROW_CONFIG_DIR/backups"
 export SPARROW_LAB_NAME="TRaIL"
 
 #export SPARROW_SITE_CONTENT="$PROJECT_DIR/site-content"
 
-pipeline="$PROJECT_DIR/import-pipeline"
+pipeline="$SPARROW_CONFIG_DIR/import-pipeline"
 export SPARROW_COMMANDS="$pipeline/bin"
 
 # Keep volumes for this project separate from those for different labs
 export COMPOSE_PROJECT_NAME="${SPARROW_LAB_NAME}"
-export SPARROW_DATA_DIR="$PROJECT_DIR/TRaIL-Data"
+export SPARROW_DATA_DIR="$SPARROW_CONFIG_DIR/TRaIL-Data"
 
-overrides="${0:h}/sparrow-secrets.sh"
+overrides="${SPARROW_CONFIG_DIR}/sparrow-secrets.sh"
 [ -f "$overrides" ] && source "$overrides"
